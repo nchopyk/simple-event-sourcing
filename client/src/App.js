12171cafdbc715526a8f96fab015eb1f47263e74
Nazar Chopyk <61637619+naz-olegovich@users.noreply.github.com>
2021-04-11 15:14:57 +0300

    Final code

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './app.css'
+
+import EventSourcing from "./EventSourcing";
+
+
+function App() {
+
+    return (
+        <div>
+            <EventSourcing/>
+        </div>
+    )
+}
+
+
+export default App;
